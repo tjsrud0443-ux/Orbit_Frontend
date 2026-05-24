040b3c47aaa0d497280ae6a1293a09a1d4282f3f
정선경 <wjdtjsrud000@naver.com>
2026-05-24 15:17:57 +0900

    조직도 구성 중

--- src/domains/departments/departments.jsx
+++ src/domains/departments/departments.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+﻿import React, { useState, useMemo } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { 
   faSearch, 
