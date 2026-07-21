27dbab884adc42a291e98eb83ffb834c93d3493a
정선경 <wjdtjsrud000@naver.com>
2026-07-22 04:00:45 +0900

    사이드바 : 직원사이드바 인사관리에 증명서 발급 신청 관리 추가

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -52,7 +52,8 @@ const generalMenuItems = [
       { name: '직급 관리', path: '/adminRank' },
       { name: '회원가입 관리', path: '/adminSignup' },
       { name: '근태 관리', path: '/adminAttendance' },
-      { name: '연차 관리', path: '/adminLeave' }
+      { name: '연차 관리', path: '/adminLeave' },
+      { name: '증명서 발급 신청 관리', path: '/adminCertRequest' }
     ]
   },
   {
